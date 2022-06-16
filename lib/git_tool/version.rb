97212e902e507f551a36dfdc47ca3109b7a086e4
Chris Moylan <chris@chrismoylan.com>
2022-06-16 15:13:14 -0500

    switching to rust

--- lib/git_tool/version.rb
+++
@@ -1,3 +0,0 @@
-module GitTool
-  VERSION = "0.1.0"
-end
--- spec/git_tool_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe GitTool do
-  it "has a version number" do
-    expect(GitTool::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
